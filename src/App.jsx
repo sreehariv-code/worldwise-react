5e39135e519b467c7aa26aa44979a0afca838928
Sreehari P V <sreehari.valsan@calpinetech.com>
2024-04-19 16:42:04 +0530

    adding useContext

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import {
   BrowserRouter as Router,
   Routes,
 } from "react-router-dom";
-import { useEffect, useState } from "react";
 
 import {
   AppLayout,
@@ -15,50 +14,29 @@ import {
   Login,
 } from "./pages";
 import { City, CityList, CountryList, Form } from "./components";
+import { CitiesProvider } from "./context/CitiesContext";
 
-const BASE_URL = "http://localhost:8000";
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const response = await fetch(`${BASE_URL}/cities`);
-        const data = await response.json();
-        setCities(data);
-        setIsLoading(false);
-      } catch (error) {
-        alert("There was an error loading data...");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
   return (
-    <Router>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="*" element={<PageNotFound />} />
-        <Route path="app" element={<AppLayout />}>
-          {/* Acts as a Redirect (Programmatic navigation using <Navigate>). "replace" is used to current element in the history stack */}
-          <Route index element={<Navigate replace to="cities" />} />
-          <Route
-            path="cities"
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
-          <Route path="cities/:id" element={<City />} />
-          <Route path="countries" element={<CountryList cities={cities} />} />
-          <Route path="form" element={<Form />} />
-        </Route>
-        <Route path="login" element={<Login />} />
-      </Routes>
-    </Router>
+    <CitiesProvider>
+      <Router>
+        <Routes>
+          <Route index element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="app" element={<AppLayout />}>
+            {/* Acts as a Redirect (Programmatic navigation using <Navigate>). "replace" is used to current element in the history stack */}
+            <Route index element={<Navigate replace to="cities" />} />
+            <Route path="cities" element={<CityList />} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </Router>
+    </CitiesProvider>
   );
 }
 
---
+++ src/context/CitiesContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useEffect, useState } from "react";
+
+const CitiesContext = createContext();
+
+const BASE_URL = "http://localhost:8000";
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const response = await fetch(`${BASE_URL}/cities`);
+        const data = await response.json();
+        setCities(data);
+        setIsLoading(false);
+      } catch (error) {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        setCities,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
